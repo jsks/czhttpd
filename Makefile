2507bd3e002bee1eefccc05636de3af284f71881
Joshua Krusell <js.shirin@gmail.com>
2018-09-17 01:23:27 +0200

    Add makefile rule to quickly start test instance of server

--- Makefile
+++ Makefile
@@ -1,8 +1,11 @@
 all: test
-.PHONY: test stress
+.PHONY: test start stress
+
+start:
+	@zsh -f tests/start.sh
 
 test:
-	@zsh -f tests/integration/test.sh -l test.log
+	@zsh -f tests/integration/test.sh -l test.log -t srv
 
 stress:
 	@zsh -f tests/stress/stress.sh -l stress.log
---
+++ tests/start.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env zsh
+
+source ${0:A:h}/utils.sh
+
+typeset -g TESTROOT=""
+typeset -g CONF="./_cz_test.conf"
+
+<<EOF > $CONF
+DEBUG=1
+typeset -ga DEBUG_TRACE_FUNCS
+DEBUG_TRACE_FUNCS=(srv)
+source $SRC_DIR/modules/debug.sh
+
+IP_REDIRECT=127.0.0.1
+MAX_CONN=12
+HTTP_KEEP_ALIVE=1
+HTTP_TIMEOUT=30
+HTTP_RECV_TIMEOUT=5
+HTTP_BODY_SIZE=16384
+CACHE=0
+EOF
+
+exec {debugfd}>&1
+
+start_server
+heartbeat
+
+wait $PID
