c1708529532e76b7824b31c0ea75dcf5bfe61218
Joshua Krusell <js.shirin@gmail.com>
2018-10-10 08:33:17 +0200

    Speed up compress module
    Avoid parsing COMPRESS_TYPES for every request and avoid subshell when
    calculating compressed file size.

--- modules/compress.sh
+++ modules/compress.sh
@@ -1,4 +1,5 @@
 # Module to compress server output using gzip
+###
 
 # Declare our module defaults
 : ${COMPRESS:=0}
@@ -8,56 +9,62 @@
 : ${COMPRESS_CACHE:=1}
 : ${COMPRESS_CACHE_DIR:=/tmp/.czhttpd-$$}
 
-function compression_init() {
-    [[ ! -d $COMPRESS_CACHE_DIR ]] && mkdir $COMPRESS_CACHE_DIR
-    [[ $COMPRESS_LEVEL != <-> ]] && { log_err "Invalid integer for COMPRESS_LEVEL"; return 1 }
-    [[ $COMPRESS_MIN_SIZE != <-> ]] && { log_err "Invalid integer for COMPRESS_MIN_SIZE"; return 1 }
-    [[ $COMPRESS_CACHE != [01] ]] && { log_err "Invalid integer for COMPRESS_CACHE"; return 1}
+if [[ $COMPRESS_LEVEL != <-> ]]; then
+    log_err "Invalid integer for COMPRESS_LEVEL"
+    return 1
+fi
 
-    function send() { compression_filter $* }
-}
+if [[ $COMPRESS_MIN_SIZE != <-> ]]; then
+    log_err "Invalid integer for COMPRESS_MIN_SIZE"
+    return 1
+fi
 
-function compression_filter() {
-    setopt null_glob
+if [[ $COMPRESS_CACHE != [01] ]]; then
+    log_err "Invalid integer for COMPRESS_CACHE"
+    return 1
+fi
 
-    if check_if_compression $1; then
-        if [[ $COMPRESS_CACHE == "1" && -f $1 ]]; then
-            local cache_file="$COMPRESS_CACHE_DIR/${1:gs/\//}.gz"
+(( COMPRESS_CACHE )) && [[ ! -d $COMPRESS_CACHE_DIR ]] && mkdir $COMPRESS_CACHE_DIR
+typeset -ga COMPRESS_TYPES_ARRAY=(${(s.,.)COMPRESS_TYPES})
 
-            if [[ $cache_file -ot $1 || ! -f $cache_file ]]; then
-                mklock $cache_file
-                gzip -$COMPRESS_LEVEL -c $1 > $cache_file
-                rmlock $cache_file
-            fi
+function send() { compress_filter $* }
 
-            return_header "200 Ok" "Content-type: ${mtype:-application:octet-stream}; charset=UTF-8" "Content-Encoding: gzip" "Content-Length: $(stat -L +size $cache_file)"
-            send_file $cache_file
+function compress_filter() {
+    if ! check_if_compression $1; then
+        __send $1; return $?
+    fi
 
-        else
-             return_header "200 Ok" "Content-type: ${mtype:-application:octet-stream}; charset=UTF-8" "Content-Encoding: gzip" "Transfer-Encoding: chunked"
-            gzip -$COMPRESS_LEVEL -c $1 | send_chunk
+    if (( COMPRESS_CACHE )) && [[ -n $1 ]]; then
+        local -a gzip_fsize
+        local cache_file="$COMPRESS_CACHE_DIR/${1:gs/\//}.gz"
+
+        if [[ ! -f $cache_file || $cache_file -ot $1 ]]; then
+            mklock $cache_file
+            gzip -$COMPRESS_LEVEL -c $1 > $cache_file
+            rmlock $cache_file
         fi
 
-        log_f "200"
+        stat -A gzip_fsize -L +size $cache_file
+
+        return_header "200 Ok" "Content-type: ${mtype:-application:octet-stream}; charset=UTF-8" \
+                      "Content-Encoding: gzip" "Content-Length: $gzip_fsize"
+        send_file $cache_file
     else
-        __send $1
+        return_header "200 Ok" "Content-type: ${mtype:-application:octet-stream}; charset=UTF-8" \
+                      "Content-Encoding: gzip" "Transfer-Encoding: chunked"
+        gzip -$COMPRESS_LEVEL -c $1 | send_chunk
     fi
+
+    log_f 200
 }
 
 function check_if_compression() {
-    [[ -z ${(SM)req_headers[accept-encoding]#gzip} || $COMPRESS == 0 ]] && return 1
+    (( ! COMPRESS )) || [[ -z ${(SM)req_headers[accept-encoding]#gzip} ]] && return 1
 
-    [[ ! -d $COMPRESS_CACHE_DIR ]] && { log_err "Compression cache directory does not exist"; return 1 }
-
-    if [[ -f $1 ]]; then
-        for i in ${(s.,.)COMPRESS_TYPES}; [[ $mtype == $i ]] && break
-
-        if [[ $? != 0 ]] || (( $fsize < $COMPRESS_MIN_SIZE )); then
-            return 1
-        fi
+    if [[ -n $1 ]]; then
+        (( fsize < COMPRESS_MIN_SIZE )) ||
+            [[ -z ${(SM)req_headers[accept-encoding]#gzip} ]] && return 1
     fi
 
     return 0
 }
-
-compression_init
